cdfe65143602cecb4e94c373440f6dc17290ad69
Stefan Hundhammer <shundhammer@suse.de>
2021-09-23 12:57:38 +0200

    Backported -03 changes

--- src/dasdfmt_progress_02.rb
+++ src/dasdfmt_progress_02.rb
@@ -17,51 +17,62 @@ module Yast
     include Yast::Logger
 
     TIMEOUT_MILLISEC = 10
-    MAX_PROGRESS = 42
 
-    def initialize(total_cyl)
+    def initialize
       textdomain "s390"
-      @total_cyl = total_cyl
       @cyl = 0
       @progress = 0 # percent
     end
 
-    def run
+    def run_demo(total_cyl:, max_progress: 100)
+      @total_cyl = total_cyl
+
+      open
+      demo_event_loop(max_progress)
+      close
+    end
+
+    def open
       UI.OpenDialog(dialog_content)
-      demo_event_loop # Runs until 42%
+    end
+
+    def close
       UI.CloseDialog
     end
 
     def dialog_content
-      MinSize(
-        60, 15,
-        MarginBox(1, 0.45, dialog_vbox)
-      )
-    end
-
-    def dialog_vbox
-      VBox(
+      MarginBox(
+        1, # left and right margin
+        0.45, # top and bottom margin; NCurses rounds this down to 0
+        VBox(
           Heading(_("Formatting DASDs")),
-          tables,
+          MinHeight(7, tables),
           VSpacing(1),
-          ProgressBar(Id(:progress_bar), _("Total Progress"), 100, 0),
+          MinWidth(
+            65,
+            ProgressBar(Id(:progress_bar), _("Total Progress"), 100, 0)
+          ),
           VSpacing(1),
           PushButton(Id(:abort), _("Abort"))
         )
+      )
     end
 
     def tables
       HBox(
-        HWeight(
-          60,
+        MinWidth(
+          26,
           VBox(
             Left(Label(_("In Progress"))),
-            in_progress_table
-          )
+            in_progress_table)
         ),
-        HSpacing(5),
-        HWeight(
-          40,
+        HSpacing(8),
+        # Multiple HStretches to take up more excess width upon resize
+        # (each table and each HStretch gets the same share)
+        HStretch(),
+        HStretch(),
+        MinWidth(
+          15,
           VBox(
             Left(Label(_("Done"))),
             done_table
@@ -73,22 +84,29 @@ module Yast
     def in_progress_table
       Table(
         Id(:in_progress_table),
-        Header(_("Device"), Right(_("Cyl.")), Right(_("Tot. Cyl"))),
+        Header(
+          ("Device"),
+          Right(_("Cyl.") + ' ' * 6) # reserve some space for more digits
+        ),
         in_progress_items
       )
     end
 
     def in_progress_items
       [
-        Item(Id(:dasda), "/dev/dasda", @cyl, 200),
-        Item(Id(:dasdb), "/dev/dasdb", @cyl, 420),
-        Item(Id(:dasdc), "/dev/dasdc", @cyl, 300),
-        Item(Id(:dasdd), "/dev/dasdd", @cyl, 200),
-        Item(Id(:dasde), "/dev/dasde", @cyl, 200),
-        Item(Id(:dasdf), "/dev/dasdf", @cyl, 200),
-        Item(Id(:dasdg), "/dev/dasdg", @cyl, 200),
-        Item(Id(:dasdh), "/dev/dasdh", @cyl, 200)
+        Item(Id(:dasda), "/dev/dasda", format_cyl(@cyl, 200)),
+        Item(Id(:dasdb), "/dev/dasdb", format_cyl(@cyl, 420)),
+        Item(Id(:dasdc), "/dev/dasdc", format_cyl(@cyl, 300)),
+        Item(Id(:dasdd), "/dev/dasdd", format_cyl(@cyl, 200)),
+        Item(Id(:dasde), "/dev/dasde", format_cyl(@cyl, 200)),
+        Item(Id(:dasdf), "/dev/dasdf", format_cyl(@cyl, 200)),
+        Item(Id(:dasdg), "/dev/dasdg", format_cyl(@cyl, 200)),
+        Item(Id(:dasdh), "/dev/dasdh", format_cyl(@cyl, 200))
       ]
+    end
+
+    def format_cyl(current_cyl, total_cyl)
+      "#{current_cyl}/#{total_cyl}"
     end
 
     def done_table
@@ -105,12 +123,31 @@ module Yast
       ]
     end
 
-    def demo_event_loop
+    # Event handler (not a loop!) for the real application.
+    #
+    # This should be called in regular intervals during listening for progress
+    # output of the dasdfmt command so the application remains responsive; in
+    # particular for responding to the user hitting the "Abort" button. This is
+    # not just a luxury: If a formatting operation is stuck, the user needs to
+    # be able to interrupt it and start over.
+    def event_handler
+      id = UI.PollInput
+      case id
+      when :abort, :cancel # :cancel is WM_CLOSE
+        # TO DO: Open a confirmation popup asking if the user really wants to
+        # abort the ongoing formatting operation.
+        return :cancel
+      end
+    end
+
+    # Event loop just for this demo. Not needed or desired in the real
+    # application.
+    def demo_event_loop(max_progress = 100)
       loop do
         id = UI.TimeoutUserInput(TIMEOUT_MILLISEC)
         case id
         when :timeout
-          update_cyl(@cyl + 1 ) if @progress < MAX_PROGRESS
+          update_cyl(@cyl + 1 ) if @progress < max_progress
 
         when :abort, :cancel # :cancel is WM_CLOSE
           # Break the loop
@@ -128,8 +165,23 @@ module Yast
     def update_cyl(cyl)
       @cyl = cyl
       update_progress(100 * @cyl / @total_cyl)
+      # Updating just some DASDs just for the demo
+      update_cyl_cell(Id(:dasdb), cyl, 420)
+      update_cyl_cell(Id(:dasdc), cyl, 300)
+    end
+
+    # Update the cylinder cell for one item of the "In Progress" table.
+    #
+    # @param item_id [Term] ID of the table item to update
+    # @param cyl [Integer] Current cylinder of that DASD
+    # @param total_cyl [Integer] Total number of cylinders of that DASD
+    #
+    def update_cyl_cell(item_id, cyl, total_cyl)
+      return if cyl > total_cyl
+
+      UI.ChangeWidget(Id(:in_progress_table), Cell(item_id, 1), format_cyl(cyl, total_cyl))
     end
   end
 end
 
-Yast::DasdFmtProgress.new(420).run
+Yast::DasdFmtProgress.new.run_demo(total_cyl: 420, max_progress: 42)
